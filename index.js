3cdf4c7c9e1bf96c2879efb1199e6643f18a02d8
hadidbilla <hadidbilla449@gmail.com>
2023-10-31 23:49:43 +0600

    added change

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const index = express()
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoutes = require("./api/routes/user.route")
+
+const port = process.env.PORT || 6000;
+index.listen(port, () => {
+  console.log(`API listening on PORT ${port} `)
+})
 
 mongoose.connect("mongodb+srv://hadidbilla449:01749804707hb@cluster0.9xogebn.mongodb.net/?retryWrites=true&w=majority",{
   useNewUrlParser: true,
@@ -12,10 +17,10 @@ mongoose.connect("mongodb+srv://hadidbilla449:01749804707hb@cluster0.9xogebn.mon
 })
 
 mongoose.Promise = global.Promise;
-app.use(bodyParser.json());
+index.use(bodyParser.json());
 
 
-app.use((req, res, next) => {
+index.use((req, res, next) => {
   console.log("called")
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -30,20 +35,20 @@ app.use((req, res, next) => {
 });
 
 //root route
-app.get("/", (req, res) => {
+index.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/user", userRoutes)
+index.use("/user", userRoutes)
 
 
-app.use((req, res, next) => {
+index.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
 
-app.use((error, req, res, next) => {
+index.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
     error: {
@@ -51,4 +56,4 @@ app.use((error, req, res, next) => {
     }
   });
 });
-module.exports = app;
+module.exports = index;
