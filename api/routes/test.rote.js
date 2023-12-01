052c956c562b061c26cfe179d06ed85f2b71b7d3
hadidbilla <hadidbilla449@gmail.com>
2023-12-01 16:21:00 +0600

    Refactor test route to use async/await

--- api/routes/test.rote.js
+++ api/routes/test.rote.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const mongoose = require("mongoose")
 const Test = require("../models/test.model")
 
-router.get('/', (req, res, next) => {
-  const allT
+router.get('/', async (req, res, next) => {
+  try{
+
+    const tests = await Test.find({})
+    res.status(200).json({
+      tests
+    })
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: e
+    })
+  }
+    
 })
